2edbd097217048c24094e8ef5a2f5840955ddc49
frank-Mwangi <113598876+frank-Mwangi@users.noreply.github.com>
2023-06-22 09:49:11 +0300

    Adding quote author

--- script.js
+++ script.js
@@ -4,48 +4,38 @@ var textPosition = 0;
 var flag = true;
 var destination = document.getElementById("typedtext");
 
-window.addEventListener('load', typeWriter);
+window.addEventListener("load", typeWriter);
 
-function loadQuote()
-{
-    const url = "https://api.quotable.io/random";
+function loadQuote() {
+  const url = "https://api.quotable.io/random";
 
-    fetch(url)
-
-    .then (response =>{
-        if(response.ok)
-            return response.json();
-        else
-            console.log(response.status);
+  fetch(url)
+    .then((response) => {
+      if (response.ok) return response.json();
+      else console.log(response.status);
     })
 
-    .then(data=>{
-        quoteArray[index] = data.content;
-    })
+    .then((data) => {
+      quoteArray[index] = data.content + " ~ " + data.author;
+    });
 }
 
-function typeWriter()
-{
-    if(flag)
-    {
-        loadQuote();
-        quoteArray[index] += ' ';
-        flag = false;
-    }
+function typeWriter() {
+  if (flag) {
+    loadQuote();
+    quoteArray[index] += " ";
+    flag = false;
+  }
 
-    destination.innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE<span>';
+  destination.innerHTML =
+    quoteArray[index].substring(0, textPosition) + "<span>\u25AE<span>";
 
-    if(textPosition++ != quoteArray[index].length)
-    {
-        setTimeout('typeWriter()', 100);
-    }
-    else
-    {
-        quoteArray[index] = ' ';
-        setTimeout('typeWriter()', 3000)
-        textPosition = 0;
-        flag = true;
-
-    }
+  if (textPosition++ != quoteArray[index].length) {
+    setTimeout("typeWriter()", 100);
+  } else {
+    quoteArray[index] = " ";
+    setTimeout("typeWriter()", 3000);
+    textPosition = 0;
+    flag = true;
+  }
 }
-
